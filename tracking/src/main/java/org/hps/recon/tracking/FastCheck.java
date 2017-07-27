5ad2ff7eccc696849113476841d2a6a3f1d78548
Miriam Diamond <Miriam Diamond>
2017-07-27 15:40:57 -0700

    implement proper MSerror in FastCheck

--- tracking/src/main/java/org/hps/recon/tracking/FastCheck.java
+++ tracking/src/main/java/org/hps/recon/tracking/FastCheck.java
@@ -18,8 +18,27 @@ import org.lcsim.recon.tracking.seedtracker.diagnostic.ISeedTrackerDiagnostics;
  */
 public class FastCheck extends org.lcsim.recon.tracking.seedtracker.FastCheck {
 
+    private double _bfield;
+
     public FastCheck(SeedStrategy strategy, double bfield, ISeedTrackerDiagnostics diag) {
         super(strategy, bfield, diag);
+        _bfield = bfield;
+    }
+
+    private double calculateMSerror(double hit1x, double hit2x, double hit3x, double p) {
+        // angle approximation
+        // radlength=0.003417, angle = (0.0136 / p) * Math.sqrt(radlength) * (1.0 + 0.038 * Math.log(radlength))
+        double angle = 0.00062343 / p;
+
+        double dist1 = hit1x - hit2x;
+        double dist2 = hit2x - hit3x;
+        double MSerr = angle * Math.sqrt(dist1 * dist1 + dist2 * dist2);
+
+        return MSerr;
+    }
+
+    private double estimateMomentum(double slope, double rcurve) {
+        return Math.sqrt(1 + slope * slope) * _bfield * rcurve;
     }
 
     @Override
@@ -296,7 +315,9 @@ public class FastCheck extends org.lcsim.recon.tracking.seedtracker.FastCheck {
         double zpred = z0 + s[1] * slope;
 
         //  Add multiple scattering error here
-        dztot += 1.0;
+        double pEstimate = estimateMomentum(slope, rcurv);
+        double mserr = calculateMSerror(p[0][0], p[1][0], p[2][0], pEstimate);
+        dztot += super.getNSig() * mserr;
 
         // comparison of middle z to prediction including error
         if (Math.abs(zpred - z[1]) > dztot)
